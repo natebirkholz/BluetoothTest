49a9c8b4085bef1274e6b6bde3d94d461a5ceed7
natebirkholz <nbirkholz@gmail.com>
2017-06-12 08:30:26 -0700

    Removed implicilty- and force-unwrapped optionals.

--- BlutetoothTest/ViewController.swift
+++ BlutetoothTest/ViewController.swift
@@ -12,42 +12,45 @@ import UserNotifications
 
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
 
-    var timestampLabel: UILabel!
-    var timestampLegendLabel: UILabel!
-    var subscriptionLabel: UILabel!
-    var centralManager: CBCentralManager!
-    var remotePeripheral: CBPeripheral!
+    var timestampLabel: UILabel?
+    var timestampLegendLabel: UILabel?
+    var subscriptionLabel: UILabel?
+    var centralManager: CBCentralManager?
+    var remotePeripheral: CBPeripheral?
     let streamServiceUuid = CBUUID(string: "D701F42C-49E1-48E9-B6E2-3862FEB2F550")
-    var central : CBCentral!
+    var central : CBCentral?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        timestampLabel = UILabel(frame: CGRect(x: (view.bounds.width / 2) - 125, y: 100.0, width: 250.0, height: 20))
+        let timestampLabel = UILabel(frame: CGRect(x: (view.bounds.width / 2) - 125, y: 100.0, width: 250.0, height: 20))
         timestampLabel.font = UIFont.systemFont(ofSize: 17.0)
         timestampLabel.textAlignment = .center
         timestampLabel.text = "------"
         view.addSubview(timestampLabel)
+        self.timestampLabel = timestampLabel
 
-        timestampLegendLabel = UILabel(frame: CGRect(x: (view.bounds.width / 2) - 125, y: 75, width: 250.0, height: 25))
+        let timestampLegendLabel = UILabel(frame: CGRect(x: (view.bounds.width / 2) - 125, y: 75, width: 250.0, height: 25))
         timestampLegendLabel.font = UIFont.systemFont(ofSize: 20)
         timestampLegendLabel.textAlignment = .center
         timestampLegendLabel.text = "Timestamp:"
         view.addSubview(timestampLegendLabel)
+        self.timestampLegendLabel = timestampLegendLabel
 
-        subscriptionLabel = UILabel(frame: CGRect(x: (view.bounds.width / 2) - 75, y: 150, width: 150, height: 25))
+        let subscriptionLabel = UILabel(frame: CGRect(x: (view.bounds.width / 2) - 75, y: 150, width: 150, height: 25))
         subscriptionLabel.backgroundColor = UIColor.red.withAlphaComponent(0.33)
         subscriptionLabel.textAlignment = .center
         subscriptionLabel.text = "Unsubscribed..."
         view.addSubview(subscriptionLabel)
+        self.subscriptionLabel = subscriptionLabel
 
-        let manager = CBCentralManager(delegate: self, queue: nil, options: nil)
-        centralManager = manager
+        let centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
+        self.centralManager = centralManager
     }
 
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         print(central.state.rawValue)
-        centralManager.scanForPeripherals(withServices: [streamServiceUuid], options: nil)
+        centralManager?.scanForPeripherals(withServices: [streamServiceUuid], options: nil)
     }
 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
@@ -57,7 +60,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
 
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         remotePeripheral = peripheral
-        centralManager.connect(peripheral, options: nil)
+        centralManager?.connect(peripheral, options: nil)
     }
 
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
@@ -65,7 +68,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
             if let valueFrom = characteristic.value  {
                 if let timeStamp = String(data: valueFrom, encoding: .utf8) {
                     if UIApplication.shared.applicationState == .active {
-                        timestampLabel.text = timeStamp
+                        timestampLabel?.text = timeStamp
                         print("ACTIVE \(timeStamp)")
                     } else if UIApplication.shared.applicationState == .background {
                         print("BACKGROUND \(timeStamp)")
@@ -79,15 +82,19 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
 
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         if service.uuid == streamServiceUuid {
-            remotePeripheral.setNotifyValue(true, for: service.characteristics!.first!)
-            subscriptionLabel.text = "Subscribed!"
-            subscriptionLabel.backgroundColor = UIColor.green.withAlphaComponent(0.33)
+            if let characteristic = service.characteristics?.first {
+                remotePeripheral?.setNotifyValue(true, for: characteristic)
+                subscriptionLabel?.text = "Subscribed!"
+                subscriptionLabel?.backgroundColor = UIColor.green.withAlphaComponent(0.33)
+            }
         }
     }
 
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        for service in peripheral.services! {
-            peripheral.discoverCharacteristics(nil, for: service)
+        if let services = peripheral.services {
+            for service in services {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
         }
     }
 
